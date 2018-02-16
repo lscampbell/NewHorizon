24ca4768da7383b6d22572ef7123299e14185099
lee shaka campbell <lee.shaka.campbell@gmail.com>
2018-02-16 20:11:13 +0000

    Set up the Class library and the XUnit projects and add to Solution

---
+++ PlutoRover.Tests/PlutoRoverMoveRequestShould.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PlutoRover.Tests
+{
+    public class PlutoRoverMoveRequestShould
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
