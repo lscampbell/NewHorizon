171dfa3539bce08293e61034b47f82b40f93b220
lee shaka campbell <lee.shaka.campbell@gmail.com>
2018-02-16 20:44:10 +0000

    Now have the first unit test working. Move forward once.

--- PlutoRover/Rover.cs
+++ PlutoRover/Rover.cs
@@ -9,15 +9,37 @@ namespace PlutoRover
         {
             _position = new Location { Heading = "N", X = 0, Y = 0 };
         }
+
         public void Move(string[] moves)
         {
         }
-        public void Move(string move)
+
+        public void Move(string direction)
         {
+            switch(direction)
+            {
+                case "F": _position = Forward(_position);
+                    break;
+            }
         }
+
         public string GetLocation()
         {
-            return $"{_position.Heading},{_position.X},{_position.Y}";
+            return $"{_position.X},{_position.Y},{_position.Heading}";
         }
+
+        Location Forward(Location position)
+        {
+            switch(position.Heading)
+            {
+                case "N":
+                    return new Location{
+                        Heading = position.Heading,
+                        X = position.X,
+                        Y = position.Y += 1
+                    };
+            }
+            return position;
+        } 
     }
 }
